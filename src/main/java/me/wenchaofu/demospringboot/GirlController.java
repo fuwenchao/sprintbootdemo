7468fa838c41d199caa5a3aee50dea9cb03fd3f2
wenchaofu <pianzif@126.com>
2018-03-23 10:45:48 +0800

    完成课程1“

--- src/main/java/me/wenchaofu/demospringboot/GirlController.java
+++ src/main/java/me/wenchaofu/demospringboot/GirlController.java
@@ -15,42 +15,67 @@ public class GirlController {
 
     /**
      * 查询女生列表
+     *
      * @return
      */
     @GetMapping(value = "/girls")
-    public List<Girl> girllist(){
+    public List<Girl> girllist() {
         return girlRepository.findAll();
     }
 
     /**
      * 增加一个女生
+     *
      * @param cupSize
      * @param age
      * @return
      */
-    @PostMapping(value = "/addgirls")
+    @PostMapping(value = "/girladd")
     public Girl addGril(@RequestParam("cupSize") String cupSize,
-                          @RequestParam("age") Integer age){
+                        @RequestParam("age") Integer age) {
         Girl girl = new Girl();
         girl.setAge(age);
         girl.setCupSize(cupSize);
         return girlRepository.save(girl);
     }
 
-
-     @GetMapping(value = "/getgirl/{id}")
-     public Girl getgirl(@PathVariable("id") Integer id){
+    /**
+     * 通过ID查询女生
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/girl/{id}")
+    public Girl getgirl(@PathVariable("id") Integer id) {
         return girlRepository.findOne(id);
 
-     }
-
-
-
-    @PostMapping(value = "/getgirl")
-    public String updateGril(@PathVariable("id") Integer id){
-
     }
-//
-//
-//    @DeleteMapping(value = "getgirl")
+
+
+    //通过年龄查询女生
+    @GetMapping(value = "/girl/age/{age}")
+    public Girl getGirlByAge(@PathVariable("age") Integer age) {
+        return girlRepository.findByAge(age);
+    }
+
+
+    //修改一个女生
+    @PostMapping(value = "/girlupdate/{id}")
+    public Girl updateGril(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age) {
+            Girl girl = new Girl();
+            girl.setId(id);
+            girl.setAge(age);
+            girl.setCupSize(cupSize);
+            return girlRepository.save(girl);
+    }
+
+
+    //删除一个女生
+    @DeleteMapping(value = "/girldel/{id}")
+    public void girldel(@PathVariable("id") Integer id){
+        girlRepository.delete(id);
+    }
+
 }
--- src/main/java/me/wenchaofu/demospringboot/GirlRepository.java
+++ src/main/java/me/wenchaofu/demospringboot/GirlRepository.java
@@ -3,4 +3,6 @@ package me.wenchaofu.demospringboot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GirlRepository extends JpaRepository<Girl,Integer> {
+    //通过年龄查询女生
+    public Girl findByAge(Integer age);
 }
